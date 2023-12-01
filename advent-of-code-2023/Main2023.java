7f07326951674fdb16b336b6d2f761b22f336bae
Mateusz Ożóg <ozog.mat@o2.pl>
2023-12-01 20:12:02 +0100

    AoC-2023: Day 1

---
+++ advent-of-code-2023/Main2023.java
@@ -0,0 +1,10 @@
+import Day1.Day1;
+
+public class Main2023 {
+    public static void main(String[] args) {
+        // DAY 1
+        Day1 day1 = new Day1();
+        System.out.println(day1.resolvePart1());
+        System.out.println(day1.resolvePart2());
+    }
+}
---
+++ advent-of-code-2023/constants/Constants.java
@@ -0,0 +1,20 @@
+package constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Constants {
+    public static Map<String, Integer> mappedNumbers = new HashMap<>() {
+        {
+            put("one", 1);
+            put("two", 2);
+            put("three", 3);
+            put("four", 4);
+            put("five", 5);
+            put("six", 6);
+            put("seven", 7);
+            put("eight", 8);
+            put("nine", 9);
+        }
+    };
+}

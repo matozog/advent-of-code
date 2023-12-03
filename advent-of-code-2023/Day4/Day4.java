cdef90f39584b8bf0037d14bf837a33faeba3175
Mateusz Ożóg <ozog.mat@o2.pl>
2023-12-03 11:22:27 +0100

    AoC-2023: Day 3

---
+++ advent-of-code-2023/Day4/Day4.java
@@ -0,0 +1,16 @@
+package Day4;
+
+public class Day4 {
+
+    public Day4 (){
+
+    }
+
+    public int resolvePart1 () {
+        return 0;
+    }
+
+    public int resolvePart2() {
+        return 0;
+    }
+}
--- advent-of-code-2023/Main2023.java
+++ advent-of-code-2023/Main2023.java
@@ -1,6 +1,7 @@
 import Day1.Day1;
 import Day2.Day2;
 import Day3.Day3;
+import Day4.Day4;
 
 public class Main2023 {
     public static void main(String[] args) {
@@ -15,8 +16,13 @@ public class Main2023 {
 //        System.out.println(day2.resolvePart2());
 
         // DAY 3
-        Day3 day3 = new Day3();
-        System.out.println(day3.resolvePart1());
+//        Day3 day3 = new Day3();
+//        System.out.println(day3.resolvePart1());
 //        System.out.println(day3.resolvePart2());
+
+        // DAY 4
+        Day4 day4 = new Day4();
+        System.out.println(day4.resolvePart1());
+//        System.out.println(day4.resolvePart1());
     }
 }

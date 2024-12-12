49ac7ecc75fc9f6c88f57ca3dc2d11648243e710
Mateusz Ożóg <ozog.mat@o2.pl>
2024-12-12 23:45:58 +0100

    AoC 2024: Day 11

--- src/main/java/aoc2024/day11/Day11.java
+++ src/main/java/aoc2024/day11/Day11.java
@@ -3,20 +3,59 @@ package aoc2024.day11;
 import commons.FileUtils;
 import commons.Task;
 
-public class Day11 implements Task<Integer> {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day11 implements Task<Long> {
     String fileLine;
+    String[] stones;
+    int maxSteps = 75;
+    Map<Integer, Map<String, Long>> cache = new HashMap<>();
 
     public Day11() {
-        fileLine = FileUtils.getFileLinesAsStringByDelimiter("src/main/java/aoc2024/day10/input.txt", "\n");
+        fileLine = FileUtils.getFileLinesAsStringByDelimiter("src/main/java/aoc2024/day11/input.txt", "\n");
+        stones = fileLine.split(" ");
+    }
+
+    Long calculateNumberOfStones(String stone, int steps, int maxSteps) {
+        if (steps == maxSteps) return 1L;
+        long result;
+
+        cache.computeIfAbsent(steps, k -> new HashMap<>());
+
+        Long cachedValue = cache.get(steps).get(stone);
+        if (cachedValue != null) {
+            return cachedValue;
+        }
+
+        if (stone.equals("0")) {
+            result = calculateNumberOfStones("1", steps + 1, maxSteps);
+        } else if (stone.length() % 2 == 0) {
+            int halfIndex = stone.length() / 2;
+            Long firstStone = calculateNumberOfStones(stone.substring(0, halfIndex), steps + 1, maxSteps);
+            Long secondStone = calculateNumberOfStones(String.valueOf(Long.parseLong(stone.substring(halfIndex))), steps + 1, maxSteps);
+            result = firstStone + secondStone;
+        } else {
+            result = calculateNumberOfStones(String.valueOf(Long.parseLong(stone) * 2024L), steps + 1, maxSteps);
+        }
+
+        cache.get(steps).putIfAbsent(stone, result);
+
+        return result;
     }
 
     @Override
-    public Integer resolvePart1() {
-        return 0;
+    public Long resolvePart1() {
+        return Arrays.stream(stones)
+                .map(stone -> calculateNumberOfStones(stone, 0, 25))
+                .reduce(0L, Long::sum);
     }
 
     @Override
-    public Integer resolvePart2() {
-        return 0;
+    public Long resolvePart2() {
+        return Arrays.stream(stones)
+                .map(stone -> calculateNumberOfStones(stone, 0, 75))
+                .reduce(0L, Long::sum);
     }
 }
